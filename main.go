62840b7d8d8f48e4f821f6252ee5e2eb27c54f3b
thelol3882 <96868145+thelol3882@users.noreply.github.com>
2026-09-15 08:58:23 +0500

    add: first version of valid chirpy

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -44,6 +45,50 @@ func main() {
 	})
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.habdlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		var params parameters
+		if err := decoder.Decode(&params); err != nil {
+			type errResponse struct {
+				Error string `json:"error"`
+			}
+			errR := errResponse{
+				Error: "Something went wrong",
+			}
+			data, _ := json.Marshal(errR)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(data)
+			return
+		}
+
+		if len(params.Body) > 140 {
+			type errResponse struct {
+				Error string `json:"error"`
+			}
+			errR := errResponse{
+				Error: "Body message is long",
+			}
+			data, _ := json.Marshal(errR)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(data)
+			return
+		}
+
+		type validResponse struct {
+			Valid bool `json:"valid"`
+		}
+		validR := validResponse{
+			Valid: true,
+		}
+		data, _ := json.Marshal(validR)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	})
 
 	s := &http.Server{
 		Addr:         ":8080",
